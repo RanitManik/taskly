70be8bfb73ef0c9bc7ae125483a8abf5fd736d5a
RanitManik <138437760+RanitManik@users.noreply.github.com>
2024-08-16 00:55:02 +0530

    :sparkles: Add a stack navigator for history

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -30,6 +30,7 @@ export default function Layout() {
                             color={color}
                         />
                     ),
+                    headerShown: false,
                 }}
             />
             <Tabs.Screen
